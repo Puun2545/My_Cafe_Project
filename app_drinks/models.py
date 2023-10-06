94e5e728a3e18747def14d83741b85320a8935a7
PuunKotHodJumpFuc <146819894+Puun2545@users.noreply.github.com>
2023-10-06 10:20:41 +0700

    update 3
    123456Puun

--- app_drinks/models.py
+++ app_drinks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Drink(models.Model):
+    title = models.CharField(max_length=60)
+    price = models.IntegerField()
+    promotion_price = models.IntegerField(null=True)
+    is_premium = models.BooleanField(default=False)
+    promotion_end_at = models.DateTimeField(null=True)
+    description = models.TextField(null=True)
+    
+
+    
