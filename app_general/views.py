3c04432bc997b21b702e567182c3d31557848bde
PuunKotHodJumpFuc <146819894+Puun2545@users.noreply.github.com>
2023-10-07 17:58:00 +0700

    update4
    <3

--- app_general/views.py
+++ app_general/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template import loader
+from app_drinks.models import Drink
 
 # Create your views here.
 def home(request):
@@ -8,3 +9,14 @@ def home(request):
 
 def about(request):
     return render(request, 'app_general/about.html')
+
+def subs(request):
+    all_drinks = Drink.objects.order_by('-is_premium')
+    context = {'drinks': all_drinks}
+    return render(request, 'app_general/subs_form.html', context)
+
+def thanks(request):
+    return render(request, 'app_general/thanks.html')
+
+
+
