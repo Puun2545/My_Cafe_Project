2fb4f2c2279a86ea9b290257fedbc169c77c129e
Puun Vachangngern <146819894+Puun2545@users.noreply.github.com>
2023-11-15 03:51:58 +0700

    Update settings.py

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'mycafe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': os.getenv('DB_NAME'),
+        # 'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
